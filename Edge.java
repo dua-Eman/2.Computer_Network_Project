fc6da1baaba25e786f97a1b1909845322e2c20b6
Dua Eman <duaeman.726@gmail.com>
2025-07-06 11:28:39 +0500

    Delete Edge.java

--- Edge.java
+++
@@ -1,9 +0,0 @@
-public class Edge {
-    Node from, to;
-    int weight;
-    Edge(Node from, Node to, int weight) {
-        this.from = from;
-        this.to = to;
-        this.weight = weight;
-    }
-}
