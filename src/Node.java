15fbaa596c579372a77d5eb237e7fa86df50d2d3
Dua Eman <duaeman.726@gmail.com>
2025-07-09 13:07:54 +0500

    Add files via upload
    This contains all the required files to simulate the project.

---
+++ src/Node.java
@@ -0,0 +1,15 @@
+//This class represents every node in the graph, tells name and position.
+import java.awt.*;
+
+public class Node {
+    String name;
+    int x, y, r = 20;
+    Node(String name, int x, int y) {
+        this.name = name;
+        this.x = x;
+        this.y = y;
+    }
+    boolean contains(Point p) {
+        return p.distance(x, y) <= r;
+    }
+}
